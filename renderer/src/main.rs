b5aece37792cbd62189ec9afd0708bba0cb7cea8
Drake A. Morgan <353dam17@stu.olatheschools.org>
2022-10-09 12:32:47 -0500

    Update main.rs

--- renderer/src/main.rs
+++ renderer/src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
-    println!("Hello, world!");
+    //visual render start loop
+    //storing size as max width (x) and max length (y), should return as number of pixels
+    let mut fov: f32 = 70.0; //angle of sight
+    let mut screensize: [u64; 2] = getscreensize(); //chashes the current size should only run if window is changed,
+    let mut basecompare: [u64; 2] = setcentercompare(screensize); //finds the center of x and y to determine angle displacment later, should only run if window is changed
+    let i: u64 = 0;  //for x for loop
+    for _ in 0..screensize[0] {
+        //code
+        let n: u64 = 0;  //for y for loop
+        for _ in 0..screensize[1] {
+            //code
+            let angledisplacmenthorizontil: i64 = getangledisplacment(distacefromcenter);
+            let angledisplacmentvertical: i64 = getangledisplacment(distacefromcenter);
+            //itarate n for y value
+            n += 1;
+        }
+        //iterate i for x value
+        i += 1;
+    }
+}
+
+//gets the screen size
+fn getscreensize() -> [u64; 2] { //returns x and y size as and array of intagers
+    
+}
+
+fn setcentercompare(screensize: [u64; 2]) -> [u64; 2] {
+
+}
+
+fn getangledisplacment(distacefromcenter: u64) -> i64 {
+
+}
+
+fn raymarch(anglehorizontile: i64, anglevertical: i64) -> [u64; 2] { //returns the cords of the found objects pixel
+
 }
