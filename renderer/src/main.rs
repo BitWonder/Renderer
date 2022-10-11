eaf214c9b7a9ab0c31522a92b8631f76f4ca1234
Drake A. Morgan <353dam17@stu.olatheschools.org>
2022-10-11 11:13:43 -0500

    Update main.rs

--- renderer/src/main.rs
+++ renderer/src/main.rs
@@ -1,39 +1,24 @@
+struct Camera {
+    position_x: f64, //where it is on x-axis
+    position_y: f64, //where it is on y-axis
+    position_z: f64, //where it is in 3D space - set to 0 for 2D game
+    horizontal: f64, //horizontal angle - set to 0 for 2D game
+    vertical_a: f64, //vertical angle - set to 0 for 2D game
+    camera_fov: f64, //the angle of max sight of total view
+    cam_f_stop: f64, //saturation of color
+}
+
 fn main() {
-    //visual render start loop
-    //storing size as max width (x) and max length (y), should return as number of pixels
-    let mut fov: f32 = 70.0; //angle of sight
-    let mut screensize: [u64; 2] = getscreensize(); //chashes the current size should only run if window is changed,
-    let mut basecompare: [u64; 2] = setcentercompare(screensize); //finds the center of x and y to determine angle displacment later, should only run if window is changed
-    let mut windowart: [ [ [u64; 4]; screensize[0] ]; screensize[1] ]; //the pixels colors for a 2D grid in an array
-    let i: u64 = 0;  //for x for loop
-    for _ in 0..screensize[0] {
-        //code
-        let n: u64 = 0;  //for y for loop
-        for _ in 0..screensize[1] {
-            //code
-            let angledisplacmenthorizontil: i64 = getangledisplacment(distacefromcenter); //dosn't need to be mutable because the life time will run out at end of loop
-            let angledisplacmentvertical: i64 = getangledisplacment(distacefromcenter);
-            //itarate n for y value
-            n += 1;
-        }
-        //iterate i for x value
-        i += 1;
-    }
+    //see if screen size has changed and change num of drawn pixels
+    screensize = getscreensize(); //top left is 0,0
+    //get camera pos and angle
+    camera = getcamera();
 }
 
-//gets the screen size
-fn getscreensize() -> [u64; 2] { //returns x and y size as and array of intagers
-    
-}
-
-fn setcentercompare(screensize: [u64; 2]) -> [u64; 2] {
+fn getscreensize() -> [u64; 2] {
 
 }
 
-fn getangledisplacment(distacefromcenter: u64) -> i64 {
-
-}
-
-fn raymarch(anglehorizontile: i64, anglevertical: i64) -> [u64; 4] { //returns the rgba color value of pixle
+fn getcamera() -> [u64; 6] { //returns (in order) camera pos x, y, z, and camera angle horizonitle, vertical, camera FOV
 
 }
