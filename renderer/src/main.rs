5cce203c26b45e469153decd551b18961d2da50e
Drake A. Morgan <353dam17@stu.olatheschools.org>
2022-10-13 07:20:16 -0500

    Update main.rs

--- renderer/src/main.rs
+++ renderer/src/main.rs
@@ -11,11 +11,11 @@ struct Camera {
 struct Screen {
     width: u64,
     height: u64,
-    display: [ [ [f32; 3]; self.width ]; self.height],
+    display: [ [ [u8; 3]; self.width ]; self.height],
 }
 
 fn main() {
-    //see if screen size has changed and change num of drawn pixels
+    //sets the screen demenchions and stores 2D array of colors of pixels on the screen
     screen = Screen {
         width: 0,
         height: 0,
@@ -31,4 +31,21 @@ fn main() {
         camera_fov: 70.0,
         cam_f_stop: 0,
     };
+    //For each Y pixel
+        //for each X pixel
+            //find displacment angle for the ray to be cast utilizing FOV
+            //Ray march to find color in 3D space and return RGB value to the display in memory -- struct Screen
+}
+
+//ray march function entry point requireing position in 2D optional Z and optional angle (if doing 3D, requires angle)
+fn raymarch(posx: u64, posy: u64, posz: u64, angv: f64, angh: f64) -> [u8; 3] {
+    //loop until distance to object unknown object is x < 0.1*10^-5 (really small number)
+        //find distance to closest object
+        //cast a ray that distance
+    //get RGB value from object at the position
+    //find nearest light sources & main sources -- like a sun or nuke
+    //do prev loop that should be a funtion of it's own
+    //if return pos isn't within 2% of light source position then darken by calculated amount based on brightness of source
+    //else saturate and apply approprite color changes based on source
+    //return the rbg value
 }
